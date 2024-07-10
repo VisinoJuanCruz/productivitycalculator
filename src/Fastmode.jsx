d55a6ee43591f1e4ac5b4739c01630e0d3e436b4
visinodeveloper <visinodeveloper@gmail.com>
2024-07-10 13:58:10 -0300

    refactoring

--- src/Fastmode.jsx
+++ src/Fastmode.jsx
@@ -5,6 +5,8 @@ function Fastmode(){
     const [itemAvg, setItemAvg] = useState(0)
     const [cantEmployees, setCantEmployees] = useState(0)
     const [cantProduc, setCantProduc] = useState(0)
+    const [employeesKitchen, setEmployeesKitchen] = useState(1)
+   
 
     const TABLEKITCHEN = ["Parrillero","Iniciador linea1","Ensamblador linea1","Iniciador Linea 2","Productos fritos","Ensamblador Linea 2","Parrillero", "Asistente Linea 1", "Asistente Linea 2","Productos fritos"]
     const TABLESERVICE = ["Cajero","Apoyo","Papas","Torre de bebidas","Apoyo","Cajero","Apoyo","Expedidor","Apoyo"]
@@ -14,10 +16,30 @@ function Fastmode(){
 
     const submitHandler = (e) => {
         e.preventDefault();
+        console.log(e.currentTarget.cantEmployees.value,"el valor")
             
         setCantEmployees(e.currentTarget.cantEmployees.value)
         setItemAvg(e.currentTarget.itemAvg.value)
         setCantProduc(e.currentTarget.cantProduc.value)
+
+        const thresholds = [39, 77, 125, 172, 210, 258, 369, 430];
+
+        const updateEmployeesKitchen = () => {
+          setEmployeesKitchen(prevEmployeesKitchen => prevEmployeesKitchen + 1);
+        };
+  
+        for (const threshold of thresholds) {
+          console.log(threshold)
+          console.log(itemAvg)
+          console.log(cantEmployees)
+          if ( itemAvg * (cantEmployees*cantProduc) > threshold) {
+            console.log("ENTRA?")
+            updateEmployeesKitchen();
+          } else {
+            break;
+          }
+        }
+
      }
   
   return (
@@ -42,10 +64,7 @@ function Fastmode(){
           <p>Para que la productividad dé, se deben hacer un total de {cantEmployees*cantProduc} GC's</p>
           <p>Para hacer esa cantidad de tickets, vas a vender aproximadamente {(cantEmployees*cantProduc)*itemAvg} items</p>
           <p>Para eso vas a necesitar:</p>
-          <p>Empleados en cocina:  {Math.floor(((cantEmployees*cantProduc)*itemAvg)/30)}</p>
-          <p>Empleados en servicio:  {Math.floor(((cantEmployees*cantProduc)/3)/10)}  (Serían para hacer {(Math.floor(((cantEmployees*cantProduc)/3)/10)+1)*10} tickets)</p>
-          <p>Empleados en automac:  {Math.floor(((cantEmployees*cantProduc)/3)/10)}  (Serían para hacer {(Math.floor(((cantEmployees*cantProduc)/3)/10)+1)*10} tickets)</p>
-          <p>Empleados en delivery:  {Math.floor(((cantEmployees*cantProduc)/3)/10)}  (Serían para hacer {(Math.floor(((cantEmployees*cantProduc)/3)/10)+1)*10} tickets)</p>
+          <p>Empleados en cocina: {employeesKitchen}</p>
           <p>Los crew que no están posicionados en cocina se pueden rotar de puesto a conveniencia</p>
          
 
